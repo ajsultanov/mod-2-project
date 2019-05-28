02dd4de3bc133bd9231a429523a38c068085eef9
Adam Sultanov <adam.sultanov@gmail.com>
2019-05-28 11:39:17 -0400

    saving stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ApplicationRecord
   has_many :pictures
   has_many :comments
-  has_many :pictures, through: :comments  # necessary?
+  # has_many :pictures, through: :comments    # unnecessary? ask about this
+                                                # how to set up separate relationship between
+                                                # users and pictures via comments without
+                                                # goofing up the user-picture relationship
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  # conditional on signup -- validates :password_confirmation, presence: true
+
+  has_secure_password
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+[1,2,3].each do |num|
+  User.create(
+    username:"user#{num}",
+    email: "user#{num}@example.com",
+    password: "test123"
+  )
+end
+
+%w(Nature NYC Art Humor Space Travel Fun).each do |tag_name|
+  Tag.create(name: tag_name)
+end
+
+url = "http://www.defenders.org/sites/default/files/styles/large/public/dolphin-kristian-sekulic-isp.jpg"
+
+Picture.create(
+  url: url,
+  title: "Saw a dolphin!",
+  user_id: User.first.id
+)
+
+PictureTag.create(
+  picture_id: 1,
+  tag_id: 1
+)
+
+Comment.create(
+  content: "Incredible!!!!",
+  user_id: 2,
+  picture_id: 1
+)
